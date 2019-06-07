9908b87fcd6fe2e36695eb7ea425e8737e0b5085
Thomas Minier <tminier01@gmail.com>
2019-06-07 13:03:36 +0200

    build website from void

--- src/void.js
+++ src/void.js
@@ -1,10 +1,10 @@
-/* file : app.js
+/* file : void.js
 MIT License
 
 Copyright (c) 2019 Thomas Minier
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
+of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
@@ -13,7 +13,7 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -23,33 +23,33 @@ SOFTWARE.
 */
 
 'use strict'
+const { DCTERMS, HYDRA, SAGE, SD, RDF, RDFS } = require('./rdf.js')
+const request = require('request')
 
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
-const fetchVoID = require('./void.js')
-
-
-const url = process.argv[2]
-fetchVoID(url)
-  .then(voID => {
-    const app = express()
-
-    app.set('view engine', 'pug')
-    app.set('views', path.join(__dirname, 'views'))
-    app.use(cors())
-
-    app.use(express.static(path.join(__dirname, 'static')))
-    app.use(express.static(path.join(__dirname, '../node_modules')))
-
-
-    app.get('/', function (req, res) {
-      res.render('home', { year: new Date().getFullYear(), serverURL: url, voID })
-    })
-
-
-    app.listen(3000, function () {
-      console.log('Sage Web listening on port 3000!')
+// fetch the VoID description of a server
+function fetchVoID (url) {
+  if (url.endsWith('/')) {
+    url = url.substring(0, url.length - 1)
+  }
+  return new Promise((resolve, reject) => {
+    request({
+      url: `${url}/void`,
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    }, (err, res) => {
+      if (err) {
+        reject(err)
+      }
+      const body = JSON.parse(res.body)
+      const m = new Map()
+      body.forEach(entity => {
+        m.set(entity['@id'], entity)
+      })
+      resolve(m)
     })
   })
-  .catch(console.error)
+}
+
+module.exports = fetchVoID
